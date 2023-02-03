95bb5dae076797c9e41977e7cc1753cf5c8b7151
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-02-03 08:23:23 +0200

    attempt

---
+++ index.js
@@ -0,0 +1,10 @@
+let intropage=document.getElementById("intro-page");
+let mainpage=document.getElementById("main-page");
+
+
+ setTimeout(function() {
+    intropage.classList.add("hidden");
+    mainpage.classList.remove("hidden");
+  }, 2000);
+
+
